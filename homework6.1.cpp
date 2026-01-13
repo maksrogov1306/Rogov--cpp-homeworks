2b3b4385809faf6f24779c7c92a6b55b002c8c61
maksrogov1306 <maksrogov1306@gmail.com>
2026-01-13 08:13:41 +0500

    Add files via upload

---
+++ homework6.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+// 37 % 3 == 1 => вариант 1 - сумма элементов
+
+int main()
+{
+    srand(time(NULL)); // объявление генератора случайных чисел на основе времени
+    int array[5], arraySum = 0, count = 0; // объявление переменных - массива, суммы его элементов, счётчика для цикла
+
+    cout << "Array:\n"; // объявление вывода элементов массива
+
+    for (count; count < 5; count++) // цикл для заполнения массива и вывода элементов
+    {
+        array[count] = rand(); // генерация и заполение ячейки
+        cout << array[count] << " "; // вывод элемента с разделителем пробелом
+        arraySum += array[count]; // формирование суммы элементов
+    }
+
+    cout << "\nSum: " << arraySum; // вывод итоговой суммы
+
+    return 0;
+}
---
+++ homework6.3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+// 37 % 3 == 1 => вариант 1 - длина строки
+
+int main()
+{
+    string anyStr; // объявление переменной
+
+    cout << "Enter string: "; // ввод строки
+    cin >> anyStr;
+
+    cout << "Length: " << anyStr.length(); // вывод длины строки
+
+    return 0;
+}

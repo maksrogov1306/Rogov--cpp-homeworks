f7d7daee9311653b362299f3e3a243ae057efcb5
maksrogov1306 <maksrogov1306@gmail.com>
2026-01-13 06:11:20 +0500

    Add files via upload

---
+++ homework3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+// 37 % 3 == 1 => вариант 1 - остаток от деления
+
+int main()
+{
+	int numOne, numTwo; // объявление переменных
+
+	cout << "Enter the 1st number: "; // приглашение пользователя для ввода
+	cin >> numOne; // ввод делимого
+
+	cout << "Enter the 2nd number: "; // аналогично
+	cin >> numTwo; // ввод делителя
+
+	int result = numOne % numTwo; // рассчёт остатка от деления
+	cout << "Result: " << result; // вывод результата
+
+	return 0;
+}

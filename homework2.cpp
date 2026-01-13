d8ccfcad6fe0e483b166a08d196d2618f4b6bf99
maksrogov1306 <maksrogov1306@gmail.com>
2026-01-13 05:30:48 +0500

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string userName; // объявление переменной
+
+	cout << "Enter your name:"; // приглашение пользователя
+	cin >> userName; //ввод имени
+	cout << "Hello, " << userName; // вывод приветствия
+
+	return 0;
+}

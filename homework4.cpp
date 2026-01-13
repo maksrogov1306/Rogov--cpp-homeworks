785f8b7e8be3baf11f9cc16228b652d5c3ea3a6b
maksrogov1306 <maksrogov1306@gmail.com>
2026-01-13 07:00:07 +0500

    Add files via upload

---
+++ homework4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+// 37 % 2 == 1 => вариант 1 - символ и его код
+
+int main()
+{
+	char sym; // объявление переменной
+	cout << "Enter symbol: "; // приглашение для ввода
+	cin >> sym; // ввод
+
+	cout << "Symbol:" << sym << endl; // вывод символа
+	cout << "Code:" << static_cast<int>(sym) << endl; // вывод ASCII символа
+	cout << "Code of next symbol:" << static_cast<int>(sym + 1) << endl; // вывод ASCII след символа
+	cout << "Next symbol:" << static_cast<char>(sym + 1); // вывод след символа
+
+	return 0;
+}
---
+++ homework5.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+// 37 % 3 == 1 => вариант 1 - площадь круга
+
+int main()
+{
+    double radius, pi = 3.14; // объявление переменной и параметра pi
+
+    cout << "Enter radius: "; // приглашение для ввода радиуса
+    cin >> radius; // ввод радиуса
+
+    double S = pi * radius * radius; // формула рассчёта площади
+    cout << "Area: " << S; // вывод площади
+
+    return 0;
+}
